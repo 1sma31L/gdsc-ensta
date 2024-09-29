d79d8b52ebe49ae3b095e872ce547e95b9ab3f30
ismail <ismailismo78@gmail.com>
2024-09-29 21:09:58 +0100

    Update Home component to display "Hello GDSC!" with additional content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-	return <div>Hello GDSC!</div>;
+	return (
+		<main className="flex justify-center items-center w-full h-screen">
+			<h1 className="text-4xl">Hello GDSC!</h1>
+			<p className="text-sm">made by ismail &gt;3</p>
+		</main>
+	);
 }
